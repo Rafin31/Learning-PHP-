72181090e6eb7c2aa6d956964285a3c301db7d78
asifhossain <asifhossain976@gmail.com>
2021-02-18 21:09:37 +0600

    TASK 1 -> TASK C

---
+++ MID_LAB_TASK_PHP_05/Task_1/Task_C/main.php
@@ -0,0 +1,19 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Name Input</title>
+</head>
+<body>
+	<form method="POST" action="main.php">
+		<fieldset>
+			<legend>Name</legend>
+			 <input type="text" name="name" value="<?php if(isset($_POST['name'])){ echo $_POST['name'];} ?>"> <br>
+			<input type="submit" name="submit" value="Submit">
+		</fieldset>
+	</form>
+	
+</body>
+</html>
+
